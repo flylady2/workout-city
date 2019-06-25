76365b429c4717b4683bdaf52174064a7e76e7a3
Leslie Stevens <flymaven@protonmail.com>
2019-06-25 17:12:11 -0500

    add nested routes for workouts under trainers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :exercises
   resources :gyms
-  resources :workouts
+
   resources :clients
   resources :trainers
+  resources :trainers, only: [:show] do
+    resources :workouts, only: [:new, :create, :edit, :update, :destroy]
+  end
+  resources :workouts, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
