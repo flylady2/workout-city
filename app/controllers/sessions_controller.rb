1ac7145305c269dc64f12e3ea46cda6282937e7d
Leslie Stevens <flymaven@protonmail.com>
2019-06-28 15:55:01 -0500

    build signup and login for client in sessions controller:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,19 @@ def create_trainer
     redirect_to 'trainer/login'
   end
 
+  def new_client
+    @client = Client.new
+    render :client/login
+  end
+
+  def create_client
+    @client = Client.find_by(username: params[:client][:username])
+    if @client && @client.authenticate(params[:client][:password])
+      session[:user_id] = @client.id
+      redirect_to client_path(@client)
+    else
+      redirect_to 'client/login'
+    end
+
+
 end
