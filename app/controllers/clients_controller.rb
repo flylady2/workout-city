d3569277aa606d8ed74e0d09dac59c67d25ff82c
Leslie Stevens <flymaven@protonmail.com>
2019-06-28 15:55:39 -0500

    build new, create and show routes for client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,30 @@
 class ClientsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(client_params)
+    #byebug
+    if @client.save
+      session[:user_id] = @client.id
+      redirect_to client_path(@client)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @client = Client.find_by(id: params[:id])
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:username, :email, :password, :age)
+  end
 end
