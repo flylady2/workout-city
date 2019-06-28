755282d3c66ed8a439860884ee5b898bdc1d9f93
Leslie Stevens <flymaven@protonmail.com>
2019-06-27 21:31:15 -0500

    build login route for trainers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
 def index
 end
 
-def new
+#load trainer signup form
+def new_trainer
   @trainer = Trainer.new
+  render :trainer/login
 end
+
+#trainer signup
+def create_trainer
+  @trainer = Trainer.find_by(username: params[:trainer][:username])
+  if @trainer && @trainer.authenticate(params[:trainer][:password])
+    session[:user_id] = @trainer.id
+    redirect_to trainer_path(@trainer)
+  else
+    redirect_to 'trainer/login'
+  end
 
 end
