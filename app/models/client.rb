8c25dc194cc268d8f5e15a2179c3df8bda0844d5
Leslie Stevens <flymaven@protonmail.com>
2019-06-28 15:55:59 -0500

    add validations to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,7 @@ class Client < ApplicationRecord
   has_secure_password
   has_many :workouts
   has_many :trainers, through: :workouts
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
