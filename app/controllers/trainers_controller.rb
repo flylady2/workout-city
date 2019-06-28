47dc19ddc3c185c3950a43d0b11178964c59c548
Leslie Stevens <flymaven@protonmail.com>
2019-06-27 21:32:29 -0500

    build new, create and show methods

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -3,8 +3,38 @@ class TrainersController < ApplicationController
 def index
 end
 
+#loading the signup form
 def new
   @trainer = Trainer.new
 end
+
+#signup
+def create
+  #byebug
+  #@trainer = Trainer.new(trainer_params)
+  if params[:gym_id] && gym = Gym.find_by_id(params[:gym_id])
+    @trainer = gym.build_trainer(trainer_params)
+  else
+    @trainer = Trainer.new(trainer_params)
+  end
+  #byebug
+  if @trainer.save
+    session[:user_id] = @trainer.id
+    redirect_to trainer_path(@trainer)
+  else
+    render :new
+  end
+end
+
+def show
+  @trainer = Trainer.find_by(id: params[:id])
+end
+
+private
+
+def trainer_params
+  params.require(:trainer).permit(:username, :password, :gym_id)
+end
+
 
 end
