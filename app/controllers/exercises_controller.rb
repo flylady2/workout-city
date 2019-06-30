d563b1d263ee75086988443f39480eae249fcfaa
Leslie Stevens <flymaven@protonmail.com>
2019-06-30 16:17:31 -0500

    build exercise routes and views

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,29 @@
 class ExercisesController < ApplicationController
+
+  def index
+    @exercises = Exercise.all
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to exercise_path(@exercise)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @exercise = Exercise.find_by(id: params[:id])
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :target)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
     resources :exercises, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :workouts, only: [:index, :show]
-  resources :exercises, only: [:index, :show]
+  resources :exercises, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
