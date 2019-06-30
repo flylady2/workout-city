4ae0e5aa1fadf70aa6365545354cfb68925961be
Leslie Stevens <flymaven@protonmail.com>
2019-06-30 17:29:13 -0500

    build workout routes and new form

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,21 @@
 class WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    if params[:trainer_id] && trainer = Trainer.find_by_id(params[:trainer_id])
+      @workout = trainer.workouts.build
+    end
+  end
+
+  def create
+  end
+
+  private
+  def workout_params
+    params.require(:workout).permit(:name, :trainer_id, :date)
+  end
+
 end
