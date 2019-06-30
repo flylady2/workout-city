2fe3bcf3ad490b3295f61cfe8ab8f1791fbd2905
Leslie Stevens <flymaven@protonmail.com>
2019-06-30 16:45:01 -0500

    define current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    Trainer.find_by_id(session[:user_id]) || Client.find_by_id(session[:user_id])
+  end
+
 end
